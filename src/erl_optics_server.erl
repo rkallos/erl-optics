68b65c5b9dc2c5c6dc33e552830aa6d7ddbade53
Fabien Lamarche-Filion <fabien.lf@partner.samsung.com>
2019-02-28 10:25:26 -0500

    Check mode when handling info and removing prototyping functions

--- src/erl_optics_server.erl
+++ src/erl_optics_server.erl
@@ -5,8 +5,8 @@
 
 -export([
     init/1,
-    handle_call/3,
     handle_cast/2,
+    handle_call/3,
     handle_info/2,
     terminate/2,
     code_change/3
@@ -16,12 +16,6 @@
     start_link/0,
     stop/0
 ]).
-
-%for prototyping only
--export([
-    start/0,
-    test_update/0,
-    start_test/1]).
 
 -define(SERVER, ?MODULE).
 
@@ -44,9 +38,6 @@ start_link() ->
 stop() ->
     gen_server:cast(?SERVER, stop).
 
-start_test(Interval) ->
-    gen_server:cast(?SERVER, {test_update, Interval}).
-
 %%%==========
 %%% Callbacks
 %%%==========
@@ -60,65 +51,33 @@ init(_) ->
             Interval = ?ENV(?ENV_INTERVAL, ?DEFAULT_INTERVAL),
             erl_optics:register_carbon_poller(Hostname, Port),
             timer:send_interval(Interval, carbon_poll),
-            %gen_server:cast(?SERVER, {test_update, 10}), %for testing
-            {ok, #state{mode = carbon, port = Port, addr = Hostname}, 0};
+            {ok, #state{mode = Mode, port = Port, addr = Hostname}, 0};
         blank ->
-            {ok, #state{mode = blank}, 0}
+            {ok, #state{mode = Mode}, 0}
     end.
-
-handle_call(erlang_poll, _From, State) ->
-    {reply, erl_optics:poll(), State}.
 
 handle_cast(stop, State) ->
     {stop, normal, State};
-handle_cast({test_update, Interval}, State) ->
-    timer:send_interval(Interval, test_update),
-    {noreply, State};
 handle_cast(_, State) ->
     {noreply, State}.
 
+handle_call(_, _From, State) ->
+    {reply, {error, undefined_call}, State}.
 
-handle_info(timeout, State) ->
-    {noreply, State};
-handle_info(test_update, State) ->
-    test_update(),
-    {noreply, State};
 handle_info(carbon_poll, State) ->
-    erl_optics:poll(),
-    {noreply, State};
+    Mode = State#state.mode,
+    case Mode of
+        carbon ->
+            erl_optics:poll(),
+            {noreply, State};
+        _ ->
+            {noreply, State}
+    end;
 handle_info(_, State) ->
     {noreply, State}.
 
 terminate(_Reason, _State) ->
     ok.
 
-
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
-
-
-
-%%%===================
-%%% Internal functions
-%%%===================
-
-start() ->
-    Lenses = [
-        erl_optics_lens:counter(<<"bob_the_counter">>),
-        erl_optics_lens:dist(<<"bob_the_dist">>),
-        erl_optics_lens:gauge(<<"bob_the_gauge">>),
-        erl_optics_lens:histo(<<"bob_the_histo">>, [0, 10, 20, 30, 40]),
-        erl_optics_lens:quantile(<<"bob_the_quantile">>, 0.5, 0.0, 0.01)
-    ],
-    erl_optics:start(<<"test">>, Lenses).
-
-test_update() ->
-    erl_optics:counter_inc(<<"bob_the_counter">>),
-    %erl_optics:dist_record_timing_now_us(<<"bob_the_dist">>, os:timestamp()),
-    %erl_optics:dist_record(<<"bob_the_dist">>, rand:normal()),
-    erl_optics:dist_record(<<"bob_the_dist">>, 1.0),
-    erl_optics:gauge_set(<<"bob_the_gauge">>, rand:uniform()),
-    %erl_optics:quantile_update_timing_now_us(<<"bob_the_quantile">>, os:timestamp() ),
-    erl_optics:quantile_update(<<"bob_the_quantile">>, rand:uniform()),
-    erl_optics:histo_inc(<<"bob_the_histo">>, rand:uniform() * 40.0),
-    ok.
